262cf5b668f88b01771185220c95d3122b4bf639
VladBaton <vladstalker2000@yandex.ru>
2025-01-19 11:35:32 +0300

    CHSU-0002: initial commit

---
+++ src/main/java/com/chsu/watervessels/WaterVesselsApplication.java
@@ -0,0 +1,13 @@
+package com.chsu.watervessels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterVesselsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterVesselsApplication.class, args);
+	}
+
+}
